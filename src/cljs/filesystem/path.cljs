5f549ed13c197516f5c87971c6c97299b7610ffc
Justus Adam <dev@justus.science>
2015-12-06 18:03:46 +0100

    full path library abstraction

--- src/cljs/filesystem/path.cljs
+++ src/cljs/filesystem/path.cljs
@@ -6,6 +6,17 @@
 (def pathlib (nodejs/require "path"))
 
 
-(defn get-extension [path] (.extname pathlib path))
-
-(defn join [& parts] (apply (.-join pathlib) parts))
+(defonce basename (.-basename pathlib))
+(defonce delimiter (.-delimiter pathlib))
+(defonce dirname (.-dirname pathlib))
+(defonce get-extension (.-extname pathlib))
+(defonce format (.-format pathlib))
+(defonce is-absolute (.-isAbsolute pathlib))
+(defonce normalize (.-normalize pathlib))
+(defonce parse (.-parse pathlib))
+(defonce posix (.-posix pathlib))
+(defonce relative (.-relative pathlib))
+(defonce resolve (.-resolve pathlib))
+(defonce sep (.-sep pathlib))
+(defonce win32 (.-win32 pathlib))
+(defonce join (.-join pathlib))
